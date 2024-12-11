aaaaf84c54a01680f5b52da9076420db4c42145a
Vivekhopper <130985868+Vivekhopper@users.noreply.github.com>
2024-12-11 18:52:28 +0530

    Update ContextProvider.jsx

--- src/Context/ContextProvider.jsx
+++ src/Context/ContextProvider.jsx
@@ -34,7 +34,7 @@ function ContextProvider({ children }) {
       }
 
       try {
-        const res = await axios.get("http://localhost:5050/api/auth/verify", {
+        const res = await axios.get("https://notesbackend-drfi.onrender.com/api/auth/verify", {
           headers: {
             Authorization: `Bearer ${token}`,
           },
